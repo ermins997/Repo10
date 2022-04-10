28c2a6f4b864c0d77e557ad3f562cc5f74fea1b4
Ermin Sacic <sacic_ermin97@hotmail.com>
2022-04-10 21:57:41 +0200

    small changes

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,14 +1,15 @@
-function myFunction(param) {
-    // Only change code below this line
-    var lion = {
+function myFunction(parameter) {
+  var lion = {
       name: "Simba",
-      legs: "4",
-      tails: "1",
-      roar: "roar-roar",
-    };
-    delete lion[param];
-    return lion;
-    // Only change code below this line
-  }
-  console.log(myFunction("roar")); // Change this line
-  module.exports = myFunction;
+      legs: 4,
+      tails: 1,
+      roar: "roar-roar"
+  };
+
+  delete lion[parameter];
+  return lion;
+}
+
+console.log(myFunction("roar"));
+
+module.exports = myFunction;
